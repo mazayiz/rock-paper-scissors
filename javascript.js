c93fa0eb67829a3d3658351ffe53dae386276a3b
Melvin Ramos <melvinr0114@gmail.com>
2025-01-08 18:06:23 -0800

    Finish Rock, Paper, Scissors Game

--- javascript.js
+++ javascript.js
@@ -5,24 +5,61 @@ function getComputerChoice() {
     let randomGuess = Math.floor(Math.random() * 3 +1);
     if (randomGuess == 1) {
         console.log("I choose Rock!");
+        return "rock";
     } else if (randomGuess == 2) {
         console.log("I choose Paper!");
+        return "paper";
     } else {
         console.log("I choose Scissors!");
+        return "scissors";
     }
-    console.log(randomGuess);
+    // console.log(randomGuess);
 };
 
-function  getHumanChoice() {
-    let human = prompt("Rock, Paper, Scissors?")
-    console.log(human)
+function getHumanChoice() {
+    let human = prompt("Rock, Paper, Scissors?");
+    return human;
 };
 
 function playRound(humanChoice, computerChoice) {
-    let upperCaseStr = str.toUpperCase();
-}
-  
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+    let humanLower = humanChoice.toLowerCase();
+    if (humanLower == computerChoice) {
+        console.log("This game is a draw!")
+    } else if (humanLower == "rock" && computerChoice == "paper") {
+        console.log("You Lose! Paper beats Rock.")
+        computerScore ++;
+    } else if (humanLower == "rock" && computerChoice == "scissors") {
+        console.log("You Win! Rock beats Scissors.")
+        humanScore ++;
+    } else if (humanLower == "paper" && computerChoice == "rock") {
+        console.log("You Win! Paper beats Rock.")
+        humanScore ++;
+    } else if (humanLower == "paper" && computerChoice == "scissors") {
+        console.log("You Lose! Scissors beats Paper.")
+        computerScore ++;
+    } else if (humanLower == "scissors" && computerChoice == "rock") {
+        console.log("You Lose! Rock beats Scissors")
+        computerScore ++;
+    } else if (humanLower == "scissors" && computerChoice == "paper") {
+        console.log("You Win! Scissors beats Paper.")
+        humanScore ++;
+        return;
+    }
+};
 
-playRound(humanSelection, computerSelection);
+function gameRound() {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log("Humanity: " + humanScore + " AI Overlords: " + computerScore);
+}
+
+function playGame() {
+    gameRound();
+    gameRound();
+    gameRound();
+    gameRound();
+    gameRound();
+};
+
+playGame();
